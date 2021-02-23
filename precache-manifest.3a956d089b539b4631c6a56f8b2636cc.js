b86b160811e272220db8229da630e2b8f2b03d00
Dhanushga Lionel <32670499+DhanushgaLionel@users.noreply.github.com>
2021-02-23 18:18:08 -0500

    Updates

---
+++ precache-manifest.3a956d089b539b4631c6a56f8b2636cc.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56024509016ace6436e1799e0c622393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41859fe9472731db6c85",
+    "url": "/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "d94042b0d54a51c02e01",
+    "url": "/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "2b363c1848e4ae795887",
+    "url": "/static/css/main.4a1a03d7.chunk.css"
+  },
+  {
+    "revision": "3e7669f8bc83998d7e29",
+    "url": "/static/js/2.921885da.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.921885da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41859fe9472731db6c85",
+    "url": "/static/js/3.13cfaa45.chunk.js"
+  },
+  {
+    "revision": "d94042b0d54a51c02e01",
+    "url": "/static/js/4.2187d694.chunk.js"
+  },
+  {
+    "revision": "2b363c1848e4ae795887",
+    "url": "/static/js/main.89bfda23.chunk.js"
+  },
+  {
+    "revision": "ab816a1749a991431208",
+    "url": "/static/js/runtime-main.1b6b1e4f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "1fcf0076e600a0d022fa75325bc07355",
+    "url": "/static/media/GPUStore.1fcf0076.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "96de6788fb67792cd2a27669a05dbaa4",
+    "url": "/static/media/capstone.96de6788.jpg"
+  },
+  {
+    "revision": "06ee4201ee4fb8d5f4d8b6bf01f0f28f",
+    "url": "/static/media/car4u2.06ee4201.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "8fc47679f7ded6975bdb5d8810452069",
+    "url": "/static/media/comfyhouse.8fc47679.jfif"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "53b2ad717fb383dc83d8ec5662e35fa7",
+    "url": "/static/media/cpLogo.53b2ad71.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "1ab97c443d176ca6860b95efe194a7bb",
+    "url": "/static/media/mytrack3.1ab97c44.jpg"
+  },
+  {
+    "revision": "39741664773bc71c3ec6cc8677063a04",
+    "url": "/static/media/otulogo.39741664.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "eeffab6e9272689dc7e983a6353aa474",
+    "url": "/static/media/react1.eeffab6e.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "a1d744ccc42b5265d8deae4c0a6fface",
+    "url": "/static/media/stan.a1d744cc.jfif"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "53015aad4cf940e4d341c556ac2d8014",
+    "url": "/static/media/vodafoneLogo.53015aad.png"
+  }
+]);
